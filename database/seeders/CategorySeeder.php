bc774b4d2aea9680e080acd1f16a5c23cf8b4074
mario1130 <pascualmedinamario@gmail.com>
2025-01-20 10:25:30 +0100

    Añadir migraciones yy datos a base de datos

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $category = new Category();
+        $category->name = 'renders';
+
+        $category2 = new Category();
+        $category2->name = 'tutoriales';
+
+        $category3 = new Category();
+        $category3->name = 'blender';
+
+        $category4 = new Category();
+        $category4->name = 'maya';
+
+        $category5 = new Category();
+        $category5->name = 'sketchup';
+
+        $category6 = new Category();
+        $category6->name = 'populars';
+
+        $category6 = new Category();
+        $category6->name = 'best_rated';
+
+
+        $category->save();
+        $category2->save();
+        $category3->save();
+        $category4->save();
+        $category5->save();
+        $category6->save();
+    }
+}
